438ff1f3181022a00a244dc8194af87b95437260
Arthur Brainville <ybalrid@ybalrid.info>
2016-10-26 00:30:28 +0200

    get the rendering running

--- ogreTest/sources/main.cpp
+++ ogreTest/sources/main.cpp
@@ -12,6 +12,7 @@
 #include <OgreHlms.h>
 #include <OgreHlmsCommon.h>
 #include <OgreHlmsManager.h>
+#include <OgreLight.h>
 #include <Hlms/Pbs/OgreHlmsPbs.h>
 #include <Hlms/Unlit/OgreHlmsUnlit.h>
 #include <Compositor/OgreCompositorManager2.h>
@@ -42,15 +43,19 @@ void quitOnPose(void)
 	run = false;
 }
 
+Ogre::Quaternion animation(float sec)
+{
+	return Ogre::Quaternion{ Ogre::Radian(Ogre::Math::PI + (Ogre::Math::PI * Ogre::Math::Cos(sec))), Ogre::Vector3::UNIT_Y };
+}
+
 int main(void)
 {
 	cout << "Hello world!\n";
-	auto quitThread = thread(quitOnPose);
-	quitThread.detach();
+	//auto quitThread = thread(quitOnPose);quitThread.detach();
 
 	//Create root
 	auto root = make_unique<Ogre::Root>("plugins.cfg", "ogre.cfg", "Ogre.log");
-	//Ogre::LogManager::getSingleton().setLogDetail(Ogre::LoggingLevel::LL_BOREME);
+	Ogre::LogManager::getSingleton().setLogDetail(Ogre::LoggingLevel::LL_BOREME);
 
 	//Configure root
 	root->showConfigDialog();
@@ -60,16 +65,18 @@ int main(void)
 
 	//Create a scene manager that use X threads
 	auto smgr = root->createSceneManager(Ogre::ST_GENERIC, SMGR_WORKERS, Ogre::INSTANCING_CULLING_THREADED);
+	smgr->setDisplaySceneNodes(true);
 
+	//Declare some resources
 	auto resourceGroupManager = Ogre::ResourceGroupManager::getSingletonPtr();
 	resourceGroupManager->addResourceLocation("./media/", "FileSystem", RG_MYRG);
 
-	auto hlmsManager = root->getHlmsManager();
-	if (!hlmsManager)
-	{
-		cerr << "hlms manager is null?\n";
-	}
+	//Init the HLMS
 
+	//Get the manager
+	auto hlmsManager = root->getHlmsManager();
+
+	//Set where to find the HLMS shader library
 	Ogre::String hlmsFolder = "./HLMS/";
 
 	//Define the shader library to use for HLMS
@@ -81,45 +88,69 @@ int main(void)
 	auto archiveUnlit = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Unlit/" + SL, "FileSystem", true);
 	auto archivePbs = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Pbs/" + SL, "FileSystem", true);
 	auto hlmsUnlit = OGRE_NEW Ogre::HlmsUnlit(archiveUnlit, &library);
-	auto hlmsPbs = OGRE_NEW Ogre::HlmsPbs(archivePbs, &library);
 	hlmsManager->registerHlms(hlmsUnlit);
+	auto hlmsPbs = OGRE_NEW Ogre::HlmsPbs(archivePbs, &library);
 	hlmsManager->registerHlms(hlmsPbs);
 
+	//All resources initialized
 	resourceGroupManager->initialiseAllResourceGroups();
 
 	//Create and attach a camera to the scene manager
 	auto camera = smgr->createCamera("MyCamera");
-	/*auto cameraNode = smgr->getRootSceneNode()->createChildSceneNode();
-	cameraNode->attachObject(camera);*/
 
+	//Configure the compositor for simple rendering
 	auto compositorManager = root->getCompositorManager2();
 
+	//Create a compositor workspace, and register it to the manager
 	const Ogre::IdString workspaceName{ "MyWorkspace" };
-
 	if (!compositorManager->hasWorkspaceDefinition(workspaceName))
-		compositorManager->createBasicWorkspaceDef(workspaceName, Ogre::ColourValue::Green);
+		compositorManager->createBasicWorkspaceDef(workspaceName, Ogre::ColourValue{ 0.2f, 0.3f, 0.4f });
 
+	//Give it the current scene, the camera from where to render, and use the window as output
 	auto workspace = compositorManager->addWorkspace(smgr, window, camera, workspaceName, true);
 
+	//Load an Ogre 1.x mesh
 	auto v1mesh = Ogre::v1::MeshManager::getSingleton().load("athene.mesh", Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME, Ogre::v1::HardwareBuffer::HBU_STATIC, Ogre::v1::HardwareBuffer::HBU_STATIC);
 
+	//Create a Ogre 2.x mesh and convert the old one to the new format
 	Ogre::MeshPtr v2mesh = Ogre::MeshManager::getSingleton().createManual("athene.mesh V2", RG_MYRG);
 	v2mesh->importV1(v1mesh.get(), true, true, true);
+
+	//Get rid of the old mesh
 	v1mesh->unload();
 	v1mesh.setNull();
 
+	//Create an Item (not an Entity) from the new mesh
 	auto a3dObject = smgr->createItem(v2mesh);
 	auto objectNode = smgr->getRootSceneNode()->createChildSceneNode();
 	objectNode->attachObject(a3dObject);
 	objectNode->setScale(0.1, 0.1, 0.1);
 
-	camera->setPosition({ 0, 0, 10 });
+	//Move the camera
+	camera->setPosition({ 0, 10, 20 });
+	camera->lookAt(objectNode->getPosition());
+	camera->setNearClipDistance(0.001);
+	camera->setFarClipDistance(2000);
+	camera->setAutoAspectRatio(true);
 
-	while (run)
+	auto light = smgr->createLight();
+	auto lightNode = smgr->getRootSceneNode()->createChildSceneNode();
+	lightNode->attachObject(light);
+	light->setType(Ogre::Light::LT_DIRECTIONAL);
+	light->setDirection(Ogre::Vector3(0, -1, -0.75f).normalisedCopy());
+	light->setPowerScale(Ogre::Math::PI);
+	/*light->setDiffuseColour(Ogre::ColourValue::Red);
+	light->setSpecularColour(Ogre::ColourValue::Red);*/
+
+	root->getTimer()->reset();
+	do
 	{
+		objectNode->setOrientation(animation(static_cast<float>(root->getTimer()->getMilliseconds()) / 1000.0f));
+		//cout << "frame\n";
 		Ogre::WindowEventUtilities::messagePump();
+		if (window->isClosed()) break;
 		root->renderOneFrame();
-	}
+	} while (run);
 
 	return EXIT_SUCCESS;
 }
