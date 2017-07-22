ad41742bc3339aac335c0daba6937939e4c74d12
Arthur Brainville <ybalrid@ybalrid.info>
2017-07-22 21:43:01 +0200

    change the name of SL to "ShadingLanguage"

--- ogreTest/sources/main.cpp
+++ ogreTest/sources/main.cpp
@@ -31,7 +31,7 @@ constexpr const char* const RG_MYRG{ "MY_RESOURCE_GROUP" };
 //Define the shading language for the HighLevelMaterialSystem.
 //I only want to use the OpenGL render system, so I'm just going to hardcode it as GLSL
 //Theses text variable should be set as so and don't change
-constexpr const char* const SL{ "GLSL" };
+constexpr const char* const ShaderLanguage{ "GLSL" };
 constexpr const char* const GL3PLUS_RENDERSYSTEM{ "OpenGL 3+ Rendering Subsystem" };
 
 //RenderWindow configuration
@@ -64,7 +64,7 @@ decltype(auto) loadV1mesh(Ogre::String meshName)
 }
 
 decltype(auto) asV2mesh(Ogre::String meshName, Ogre::String ResourceGroup = Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, Ogre::String sufix = " V2",
-						bool halfPos = true, bool halfTextCoords = true, bool qTangents = true)
+	bool halfPos = true, bool halfTextCoords = true, bool qTangents = true)
 {
 	//Get the V1 mesh
 	auto v1mesh = loadV1mesh(meshName);
@@ -84,35 +84,37 @@ decltype(auto) asV2mesh(Ogre::String meshName, Ogre::String ResourceGroup = Ogre
 void declareHlmsLibrary(const Ogre::String&& path)
 {
 #ifdef _DEBUG
-	if (string(SL) != "GLSL" || string(Ogre::Root::getSingleton().getRenderSystem()->getName()) != "OpenGL 3+ Rendering Subsystem")
+	if (string(ShaderLanguage) != "GLSL" || string(Ogre::Root::getSingleton().getRenderSystem()->getName()) != "OpenGL 3+ Rendering Subsystem")
 		throw std::runtime_error("This function is OpenGL only. Please use the RenderSytem_GL3+ in the Ogre configuration!");
 #endif
-    Ogre::String hlmsFolder = path;
+	Ogre::String hlmsFolder = path;
 
-    //The hlmsFolder can come from a configuration file where it could be "empty" or set to "." or lacking the trailing "/"
-    if (hlmsFolder.empty()) hlmsFolder = "./";
-    else if (hlmsFolder[hlmsFolder.size() - 1] != '/') hlmsFolder += "/";
+	//The hlmsFolder can come from a configuration file where it could be "empty" or set to "." or lacking the trailing "/"
+	if (hlmsFolder.empty()) hlmsFolder = "./";
+	else if (hlmsFolder[hlmsFolder.size() - 1] != '/') hlmsFolder += "/";
 
-    //Get the hlmsManager (not a singleton by itself, but accessible via Root)
-    auto hlmsManager = Ogre::Root::getSingleton().getHlmsManager();
+	//Get the hlmsManager (not a singleton by itself, but accessible via Root)
+	auto hlmsManager = Ogre::Root::getSingleton().getHlmsManager();
 
-    //Define the shader library to use for HLMS
-    auto library = Ogre::ArchiveVec();
+	//Define the shader library to use for HLMS
+	auto library = Ogre::ArchiveVec();
 
-    auto archiveLibrary = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Common/" + SL, "FileSystem", true);
-    auto archiveLibraryAny = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Common/Any", "FileSystem", true);
-    auto archivePbsLibraryAny = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Pbs/Any", "FileSystem", true);
-    library.push_back(archiveLibrary);
-    library.push_back(archiveLibraryAny);
-    library.push_back(archivePbsLibraryAny);
+	auto archiveLibrary = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Common/" + ShaderLanguage, "FileSystem", true);
+	auto archiveLibraryAny = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Common/Any", "FileSystem", true);
+	auto archivePbsLibraryAny = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Pbs/Any", "FileSystem", true);
+	auto archiveUnlitLibraryAny = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Unlit/Any", "FileSystem", true);
+	library.push_back(archiveLibrary);
+	library.push_back(archiveLibraryAny);
+	library.push_back(archivePbsLibraryAny);
+	library.push_back(archiveUnlitLibraryAny);
 
-    //Define "unlit" and "PBS" (physics based shader) HLMS
-    auto archiveUnlit = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Unlit/" + SL, "FileSystem", true);
-    auto archivePbs = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Pbs/" + SL, "FileSystem", true);
-    auto hlmsUnlit = OGRE_NEW Ogre::HlmsUnlit(archiveUnlit, &library);
-    auto hlmsPbs = OGRE_NEW Ogre::HlmsPbs(archivePbs, &library);
-    hlmsManager->registerHlms(hlmsUnlit);
-    hlmsManager->registerHlms(hlmsPbs);
+	//Define "unlit" and "PBS" (physics based shader) HLMS
+	auto archiveUnlit = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Unlit/" + ShaderLanguage, "FileSystem", true);
+	auto archivePbs = Ogre::ArchiveManager::getSingletonPtr()->load(hlmsFolder + "Hlms/Pbs/" + ShaderLanguage, "FileSystem", true);
+	auto hlmsUnlit = OGRE_NEW Ogre::HlmsUnlit(archiveUnlit, &library);
+	auto hlmsPbs = OGRE_NEW Ogre::HlmsPbs(archivePbs, &library);
+	hlmsManager->registerHlms(hlmsUnlit);
+	hlmsManager->registerHlms(hlmsPbs);
 }
 
 #ifdef _WIN32
@@ -161,8 +163,8 @@ int main(void)
 
 	//Create a scene manager that use X threads
 	auto smgr = root->createSceneManager(Ogre::ST_GENERIC, SMGR_WORKERS, Ogre::INSTANCING_CULLING_THREADED);
-	smgr->setDisplaySceneNodes(true);
-	smgr->setForward3D(true, 4, 4, 5, 96, 0.1f, 200);
+	//smgr->setDisplaySceneNodes(true);
+	//smgr->setForward3D(true, 4, 4, 5, 96, 0.1f, 200);
 
 	//Declare some resources
 	auto resourceGroupManager = Ogre::ResourceGroupManager::getSingletonPtr();
@@ -173,7 +175,7 @@ int main(void)
 	declareHlmsLibrary("HLMS");
 
 	//All resources initialized
-	resourceGroupManager->initialiseAllResourceGroups();
+	resourceGroupManager->initialiseAllResourceGroups(true);
 
 	//Create and attach a camera to the scene manager
 	auto camera = smgr->createCamera("MyCamera");
@@ -183,15 +185,15 @@ int main(void)
 
 	//Create a camera, detach it from the root node and attach it to a new node
 /*	if (Ogre::SceneNode* cameraNode = camera->getParentSceneNode()) cameraNode->detachObject(camera);
-    Ogre::SceneNode* cameraNode = smgr->getRootSceneNode()->createChildSceneNode();
+	Ogre::SceneNode* cameraNode = smgr->getRootSceneNode()->createChildSceneNode();
 	cameraNode->attachObject(camera);
-    */
+	*/
 
 	//Configure the compositor for simple rendering
 	auto compositorManager = root->getCompositorManager2();
 
 	//Create a compositor workspace, and register it to the manager
-    const Ogre::String workspaceNameStr{ "MyWorkspace" };
+	const Ogre::String workspaceNameStr{ "MyWorkspace" };
 	const Ogre::IdString workspaceName{ workspaceNameStr };
 	if (!compositorManager->hasWorkspaceDefinition(workspaceName))
 		compositorManager->createBasicWorkspaceDef(workspaceNameStr, Ogre::ColourValue{ 0.2f, 0.3f, 0.4f });
@@ -237,8 +239,8 @@ int main(void)
 	cameraNode->lookAt({ 0, 0, 0 }, Ogre::Node::TransformSpace::TS_PARENT);
 */
 
-    camera->setPosition(0,5,20);
-    camera->lookAt(0,0,0);
+	camera->setPosition(0, 5, 20);
+	camera->lookAt(0, 0, 0);
 
 	//define colors for the light
 	Ogre::ColourValue warm{ 1, 0.75f, 0.75f }, cold{ 0.75f, 0.75f, 1 };
